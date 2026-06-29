9955c6bb618892338b385ea03be0b1cd8c7f8cfc
Amitsaka1 <111147203+Amitsaka1@users.noreply.github.com>
2026-06-29 12:48:38 +0530

    Create api.dart

---
+++ lib/core/constants/api.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  static const String baseUrl = 'https://ai-chat-app-tupx.onrender.com';
+  static const String chatEndpoint = '/api/chat';
+  static const String defaultProvider = 'groq';
+  static const String defaultModel = 'llama-3.3-70b-versatile';
+}
